9915978afd012d20e46e835d8b504968ad0c514a
rodedrengen <simon.588.jorgensen@gmail.com>
2020-03-18 15:31:25 +0100

    LogicFacade

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -13,13 +13,13 @@ import java.util.ArrayList;
  */
 public class LogicFacade {
 
-    public static User login( String email, String password ) throws LoginSampleException {
-        return UserMapper.login( email, password );
-    } 
+    public static User login(String email, String password) throws LoginSampleException {
+        return UserMapper.login(email, password);
+    }
 
-    public static User createUser( String email, String password ) throws LoginSampleException {
-        User user = new User(email, password, false,0);
-        UserMapper.createUser( user );
+    public static User createUser(String email, String password) throws LoginSampleException {
+        User user = new User(email, password, false, 0);
+        UserMapper.createUser(user);
         return user;
     }
 
@@ -58,23 +58,29 @@ public class LogicFacade {
     public static ArrayList<Order> getAllOrdersFromUser(int userID) throws LoginSampleException {
         return OrderMapper.getAllOrdersFromUser(userID);
     }
-    public static int getAmountOfOrders(){
+
+    public static int getAmountOfOrders() {
         return StatisticsMapper.getAmountOfOrders();
     }
-    public static int getAmountOfCupcakes(){
+
+    public static int getAmountOfCupcakes() {
         return StatisticsMapper.getAmountOfCupcakes();
     }
-    public static int getAmountOfUsers(){
+
+    public static int getAmountOfUsers() {
         return StatisticsMapper.getAmountOfUsers();
     }
-    public static int getTotalSale(){
+
+    public static int getTotalSale() {
         return StatisticsMapper.getTotalSale();
     }
-    public static ArrayList<Staistics> getLast5(){
+
+    public static ArrayList<Staistics> getLast5() {
         return StatisticsMapper.getLast5();
-
-
-    public static void removeOrder(int orderId) {
-        OrderMapper.removeOrder(orderId);
+    }
+    
+    public static void removeOrder ( int orderId){
+            OrderMapper.removeOrder(orderId);
     }
 }
+
